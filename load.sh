24e3b87749b86bd4187ed4ff38d845c535e75642
Adrian Webb <adrian.webb@coraltech.net>
2013-06-10 04:46:30 -0400

    Replacing the common_utils script with a general purpose load script that serves as a central loader for this library.

---
+++ load.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# load.sh
+#
+#-------------------------------------------------------------------------------
+# Shared functions
+
+#-------------------------------------------------------------------------------
+# Get the directory of the command passed via the first parameter.
+#
+# USAGE:> SCRIPT_DIR=`get_command_location $0`
+#
+function get_command_location()
+{
+    local SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$1"` ] && echo "`readlink "$1"`" || echo "$1")")"; pwd -P)"
+    
+    # Return the final directory found. 
+    echo "$SCRIPT_DIR"
+    return 0
+}
+
+#-------------------------------------------------------------------------------
+# Initialization
+
+if [ ! -d "$SHELL_LIB_DIR" ]
+then
+	SHELL_LIB_DIR=`get_command_location $0`
+fi
+
+#-------------------------------------------------------------------------------
+
+# Load command utilities.  This should always be loaded first.
+COMMON_COMMAND="$SHELL_LIB_DIR/command.sh"
+
+if [ -f "$COMMON_COMMAND" ]
+then
+	source "$COMMON_COMMAND"
+fi
+
+# Load file system utilities.
+COMMON_FILESYSTEM="$SHELL_LIB_DIR/filesystem.sh"
+
+if [ -f "$COMMON_FILESYSTEM" ]
+then
+	source "$COMMON_FILESYSTEM"
+fi
+
+# Load script utilities.
+COMMON_SCRIPT="$SHELL_LIB_DIR/script.sh"
+
+if [ -f "$COMMON_SCRIPT" ]
+then
+	source "$COMMON_SCRIPT"
+fi
+
+#-------------------------------------------------------------------------------
+
+# Load any validators used in parse_option() calls.
+COMMON_VALIDATORS="$SHELL_LIB_DIR/validators.sh"
+
+if [ -f "$COMMON_VALIDATORS" ]
+then
+	source "$COMMON_VALIDATORS"
+fi
